ac84e343025e5821be5d9c36c3d83c9d5963d8b0
Yu-Cheng Lin <yu-cheng_lin@comcast.com>
2021-03-12 16:21:09 -0700

    checkin changes so far

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,31 @@
 #![allow(unused_imports)]
 
 mod pm;
-use std::io::stdin;
-use std::time::Duration;
-use std::thread;
-//use std::process::Command;
+use curl::easy::Easy;
+
 fn main() {
-    let mut a = String::new();
-    loop {
-        if a.trim_end() == "bye" {
-            println!("Bye Bye!"); 
-            break; 
-        }
-        //Command::new("clear").status().unwrap();
-        //print!("{}[2J", 27 as char);
-        println!("Please tell me who you are...");
-        a.clear();
-        stdin().read_line(&mut a);
-        println!("Hello, {}!", pm::powerful_match(&mut a.trim_end()));
-        thread::sleep(Duration::from_millis(2000));
-        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-    }
-    //pm::get_url("https://google.com");
-    pm::hex2int("0xb757b1");
+    use curl::easy::Easy;
+
+// First write everything into a `Vec<u8>`
+let mut data = Vec::new();
+let mut handle = Easy::new();
+handle.url("http://url.com").unwrap();
+{
+    let mut transfer = handle.transfer();
+    transfer.write_function(|new_data| {
+        data.extend_from_slice(new_data);
+        Ok(new_data.len())
+    }).unwrap();
+    transfer.perform().unwrap();
+}
+
+// Convert it to `String`
+let body = String::from_utf8(data).expect("body is not valid UTF8!");
+println!("{:?}",body);
+    //println!("{}", json!({"token": "4768c0dacbad724ce311e761930398a6147a7f64"}).to_string());
+    //pm::hello_who();
+    //pm::get_vault_token("790353980f93e631f183c25deac0caf517d31dea","https://vault-ho.autobahn.comcast.com:8200/v1/auth/github_cdn-analytics/login");
+    //pm::get_url("https://or.vault.comcast.com/v1/auth/github_cdn-analytics/login");
+    //pm::hex2int("0xb757b1");
+    //s.7h6uMgh4zsra8lg3yyrD9KK7
 }
